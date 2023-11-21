f0917476e55b2a0eea70e8c593c75014648064d5
hector <hectorrrivera1010@gmail.com>
2023-11-21 13:00:39 -0600

    [add] test deploy 5

--- src/main.tsx
+++ src/main.tsx
@@ -51,7 +51,9 @@ const router = createBrowserRouter([
     path: '/icons',
     element: < IconsPage/>,
   }
-]);
+], {
+  basename: '/react-resources'
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
