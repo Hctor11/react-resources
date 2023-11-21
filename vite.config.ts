e804c6aecec173a5978ce772cac9ea9435ab0452
hector <hectorrrivera1010@gmail.com>
2023-11-21 12:54:10 -0600

    [add] test deploy 4

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-resources/'
+  base: 'https://hctor11.github.io/react-resources/'
 })
